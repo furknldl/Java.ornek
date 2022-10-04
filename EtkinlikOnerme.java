18cdefcbf44f8c16ab372346af883197f7cc66c5
furknldl <114441964+furknldl@users.noreply.github.com>
2022-10-04 10:22:45 +0300

    Create EtkinlikOnerme.java

---
+++ EtkinlikOnerme.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class EtkinlikOnerme {
+
+
+    public static void main(String[] args) {
+        int sicaklik;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen sıcaklık giriniz:");
+        sicaklik = input.nextInt();
+
+        if (sicaklik < 5) {
+            System.out.println("Kayak yapabilirsiniz.");
+        } else if (sicaklik >= 5 && sicaklik < 15) {
+            System.out.println("Sinemaya gidebilirsiniz.");
+        } else if (sicaklik == 15) {
+            System.out.println("Sinemaya veya pikniğe gidebilirsiniz");
+        } else if (sicaklik > 15 && sicaklik <= 25) {
+            System.out.println("Pikniğe gidebilirsiniz.");
+        } else if (sicaklik > 25) {
+            System.out.println("Yüzmeye gidebilirsiniz.");
+        }
+
+    }
+
+}
+
+
